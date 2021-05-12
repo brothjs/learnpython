d448fa95b7f45f9776f3db620ddfc0a7f1f3e465
brothjs <broth_js@hotmail.com>
2021-05-12 09:47:49 +0700

    strCompareConcatFormat.py

---
+++ strCompareConcatFormat.py
@@ -0,0 +1,17 @@
+tmp1 = "brothjs"
+tmp1 = 'brothjs'
+print(tmp1)
+
+lenOfTmp1 = len(tmp1)
+print(lenOfTmp1)
+
+if tmp1 == "brothjs":
+    print(tmp1 + " is equal " + "brothjs")
+else: 
+    print(tmp1)
+
+x, y, z = 1, 2, 3
+print("x is {},y is {},z is {}".format(x,y,z))
+
+if "hey" in "hey, yo, hi":
+    print("How are you")
