f6ae13711338d8218908d7507630bc274eabf157
brothjs <broth_js@hotmail.com>
2021-05-12 10:40:58 +0700

    booleanOperator.py

---
+++ booleanOperator.py
@@ -0,0 +1,21 @@
+print(True)
+tmp1 = True
+print(tmp1)
+
+tmp2 = len("Note") >= 4
+tmp3 = "note" == "Note"
+
+print(tmp2)
+print(tmp3)
+print(not tmp2)
+
+tmp3 = bool(1)
+print(type(tmp3))
+
+print(1/7)
+print(1.0/7)
+print(7%3)
+
+tmp4 =1
+tmp4 += 1
+print(tmp4)
