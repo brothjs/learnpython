66f526d83e7aa55b2d6b3ce005a00fd695be77b4
brothjs <broth_js@hotmail.com>
2021-05-12 10:17:16 +0700

    'Initial

---
+++ strMethod.py
@@ -0,0 +1,13 @@
+tmp = "brothjs"
+
+print(tmp)
+print(tmp.lower())
+print(tmp.upper())
+print(tmp.capitalize())
+
+#alpha numeric
+tmp1= "123@4abc"
+print(tmp1.isalnum())
+
+tmp2 = "I am Note"
+print(tmp2.find("Note"))
