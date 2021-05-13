a4f8ca898b64eb29ae1f1f71a5d9ab5c10910b2d
brothjs <broth_js@hotmail.com>
2021-05-13 09:29:34 +0700

    'Initial

---
+++ ifElse2.py
@@ -0,0 +1,13 @@
+data =5
+if data > 3 and data < 5:
+    print("data {}".format(data))
+elif data > 4:
+    print("data > 4 : {}".format(data))
+else:
+    print("else data is not > 3")
+
+if True: 
+    print("a is greater than b")
+
+print("Yes") if True else print("No")
+print("Yes") if False else print("No")
