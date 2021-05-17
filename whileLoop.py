462defac2ccedc9a9cacab21bd6b352b82384fef
brothjs <broth_js@hotmail.com>
2021-05-17 13:14:06 +0700

    'Changed

---
+++ whileLoop.py
@@ -0,0 +1,14 @@
+i=0
+while i < 10:
+    if i==3:
+        print("i=3")
+        break
+    else:
+        print(i)
+    i += 1
+
+courses = ["ang","React","flute"]
+index = 0
+while index < len(courses):
+    print(courses[index])
+    index += 1
