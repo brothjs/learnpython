fbd7fcc974a0652d1140d00d0b562fe221298a15
brothjs <broth_js@hotmail.com>
2021-05-17 13:14:36 +0700

    'Changed

---
+++ list.py
@@ -0,0 +1,16 @@
+dataList = ["ang","vuejs","react"]
+dataList2:list = ["ang","vuejs","react"]
+print(dataList)
+print(dataList2)
+print(type(dataList2))
+
+extraArray = ["rasp","flute"]
+dataList.append("python")
+dataList.extend(extraArray)
+dataList.insert(1,"Big")
+dataList.remove("vuejs")
+#dataList.clear()
+
+#del dataList
+print(dataList)
+print(dataList[2])
